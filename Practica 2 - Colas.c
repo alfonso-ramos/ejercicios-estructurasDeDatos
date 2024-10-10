cd0c4f2448f7848d85dc95c167c23be422eb727b
Poncho Ramos <86501704+alfonso-ramos@users.noreply.github.com>
2024-10-10 09:29:03 -0700

    Colas

---
+++ Practica 2 - Colas.c
@@ -0,0 +1,17 @@
+/*
+	Nombre del alumno:
+	Fecha:
+	Descripcion:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#incluce <string.h>
+
+#define print printf
+#define scan scanf
+
+
+int main(void){
+	return 0;	
+}
